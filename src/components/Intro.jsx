a20e891072f81cbaa3ffda147588e4ca0e133093
Alejandro Salazar Lizardiz <asalazarlizardiz@gmail.com>
2025-07-09 09:08:41 -0400

    Actualizando skills

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,37 +1,37 @@
 
-import { useState, useEffect } from 'react'; 
+import { useState, useEffect } from 'react';
 import { useStore } from '@nanostores/react'
-import { theme } from '../stores/themeStore' 
+import { theme } from '../stores/themeStore'
 import mainLogo from '../assets/firstLogoTransparent.webp'
 import mainLogoForDark from '../assets/logoFullWhite.webp'
-import "../styles/Intro.css" 
+import "../styles/Intro.css"
 
-export const Intro = ()=>{
-  const currentTheme = useStore(theme); 
+export const Intro = () => {
+  const currentTheme = useStore(theme);
   const [hasMounted, setHasMounted] = useState(false);
 
   useEffect(() => {
-    setHasMounted(true); 
+    setHasMounted(true);
   }, []);
 
   const imageSrc = currentTheme === "light" ? mainLogo.src : mainLogoForDark.src;
 
-  return(
+  return (
     <main className="main">
       <div className="main__container">
-          <div className="main__info-container">
-            <div className="main__img-container">
-              <img 
-                className="main__img" 
-                style={{ visibility: hasMounted ? 'visible' : 'hidden' }} 
-                src={imageSrc}
-                alt="Logo principal de LizardizNexus"
-              />
-            </div>
-            <p className="main__p">
-               ¡Hola! Soy Alejandro Salazar Lizardiz, Desarrollador Frontend especializado en transformar ideas complejas en experiencias digitales claras y efectivas. Construyo interfaces modernas con React y Astro, centradas en el usuario y la resolución de problemas. ¿Quieres ver cómo el código limpio y el diseño pueden impulsar tu proyecto? Explora mis trabajos o hablemos de tu visión.
-            </p>
+        <div className="main__info-container">
+          <div className="main__img-container">
+            <img
+              className="main__img"
+              style={{ visibility: hasMounted ? 'visible' : 'hidden' }}
+              src={imageSrc}
+              alt="Logo principal de LizardizNexus"
+            />
           </div>
+          <p className="main__p">
+            ¡Hola! Soy Alejandro Salazar Lizardiz, Desarrollador especializado en transformar ideas complejas en experiencias digitales claras, efectivas y de alto rendimiento. Construyo interfaces modernas con React, Astro y Svelte, centradas en el usuario y la resolución de problemas. ¿Quieres ver cómo el código limpio y el diseño pueden impulsar tu proyecto? Explora mis trabajos o hablemos de tu visión.
+          </p>
+        </div>
       </div>
     </main>
   )
